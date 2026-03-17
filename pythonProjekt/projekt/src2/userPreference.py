fc66bdcc767cd69882eb18f511f91ceb55771453
Kamil Styn <168219846+Styneek@users.noreply.github.com>
2026-03-17 18:33:02 +0100

    Delete pythonProjekt directory

--- pythonProjekt/projekt/src2/userPreference.py
+++
@@ -1,5 +0,0 @@
-class UserPreference:
-    def __init__(self, maxLength, maxDifficulty, sunHours = 0):
-        self.maxLength = maxLength
-        self.maxDifficulty = maxDifficulty
-        self.sunHours = sunHours
